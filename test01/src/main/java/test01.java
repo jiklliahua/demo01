47454a164f68aa005f1aa9a8660da38f96470511
fangjiahua <13555283923@qq.com>
2023-04-13 17:32:05 +0800

    第一次提交式

--- test01/src/main/java/test01.java
+++ test01/src/main/java/test01.java
@@ -1,5 +1,5 @@
 public class test01 {
     public static void main(String[] args) {
-        System.out.println("helll222qqqqqaaaaaaaaaaaa2222222222222222aaaaaaaqqqqqo");
+        System.out.println("helll222qqqqqaaaa111aaaaaaaa2222222222222222aaaaaaaqqqqqo");
     }
 }
